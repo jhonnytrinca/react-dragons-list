8532ef49342404b563c1735f23a914a2f1bc0eb9
Jhonny Trinca <jhonny.trinca@hotmail.com>
2023-02-05 19:56:04 -0300

    feat: auth service

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDp0uIrUcmBdp4AT834z8zr9BGDQlWH8uE',
-  authDomain: 'react-dragons.firebaseapp.com',
-  projectId: 'react-dragons',
-  storageBucket: 'react-dragons.appspot.com',
-  messagingSenderId: '643438132787',
-  appId: '1:643438132787:web:07134a1756fa1865e0ad46'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
