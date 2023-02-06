ebfd83e527034bba3f9fc703a575a4adfaacadd1
Jhonny Trinca <jhonny.trinca@hotmail.com>
2023-02-06 15:43:21 -0300

    feat: cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
     },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    specPattern: 'cypress/**/*.test.{jsx,tsx}',
   },
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
+  video: false,
+  screenshotOnRunFailure: false
 });
